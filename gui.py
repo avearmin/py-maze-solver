4e9bb1b3d5bf3b7a8fdd4a322b217e7c7305f2f0
Armin Radoncic <Arminradoncic1@gmail.com>
2023-06-20 18:31:12 -0400

    Add functionality for drawing cells on the window
    Introduce the `draw_cell` method to the Window class, enabling the
    drawing of cells on the window's canvas. This method takes a Cell object
    and a color as parameters. When called, it draws the Cell object onto
    the canvas with the specified color.
    
    Additionally, a new class called Cell has been implemented. The Cell
    class takes a Line object as a parameter in its constructor, storing the
    coordinates of its points in four data members. The draw method utilizes
    these coordinates to construct four separate lines, representing the
    walls of the cell.
    
    The Cell class also includes four additional data members that determine
    the presence of walls within the cell. These values play a role in
    determining which adjacent cell the maze solver will be allowed to move
    to.
    
    These additions expand the capabilities of the Window class, allowing
    for the representation of cells in the GUI.

--- gui.py
+++ gui.py
@@ -25,6 +25,9 @@ class Window:
     def draw_line(self, line, fill_color):
         line.draw(self.__canvas, fill_color)
 
+    def draw_cell(self, cell, fill_color):
+        cell.draw(self.__canvas, fill_color)
+
 class Point:
     def __init__(self, x, y):
         self.x = x
@@ -34,10 +37,44 @@ class Line:
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
-    
+
     def draw(self, canvas, fill_color):
         canvas.create_line(
             self.p1.x, self.p1.y, self.p2.x, self.p2.y, 
             fill=fill_color, width=2
+            )
+        canvas.pack()
+
+class Cell:
+    def __init__(self, line):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self.__x1 = line.p1.x
+        self.__x2 = line.p2.x
+        self.__y1 = line.p1.y
+        self.__y2 = line.p2.y
+
+    def draw(self, canvas, fill_color):
+        if self.has_left_wall:
+            canvas.create_line(
+                self.__x1, self.__y1, self.__x1, self.__y2,
+                fill=fill_color, width=2
+            )
+        if self.has_top_wall:
+            canvas.create_line(
+                self.__x1, self.__y1, self.__x2, self.__y1,
+                fill=fill_color, width=2
+            )
+        if self.has_right_wall:
+            canvas.create_line(
+                self.__x2, self.__y1, self.__x2, self.__y2,
+                fill=fill_color, width=2
+            )
+        if self.has_bottom_wall:
+            canvas.create_line(
+                self.__x1, self.__y2, self.__x2, self.__y2,
+                fill=fill_color, width=2
             )
         canvas.pack()
