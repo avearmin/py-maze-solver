5641c28357bcd24cc0fc61e95b6f2097e19cf6cb
Armin Radoncic <Arminradoncic1@gmail.com>
2023-06-28 22:00:03 -0400

    Add player input and threading to main
    I modified the main method to incorporate player input and threading
    functionality into the maze solver game. The player is able to use the
    arrow keys as inputs to travese their maze in tandem with the computer
    solving its own maze, enabling the race portion of the game.

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from gui import Window
 from maze import Maze
+from input_handler import InputHandler
+import threading
 
 def main():
-    window = Window(800, 600)
-    maze = Maze(40, 35, 13, 18, 40, 40, window)
-    maze.solve()
+    window = Window(1600, 600)
+    computer = Maze(40, 35, 13, 18, 40, 40, window, 1)
+    player = Maze(800, 35, 13, 18, 40, 40, window, 1)
+
+    threading.Thread(target=computer.solve).start()
+    input_handler = InputHandler(window, player)
+    
     window.wait_for_close()
 
 main()
