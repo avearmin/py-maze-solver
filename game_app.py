f813f1b8fe126b9fc9db188680d4cae3789e75cc
Armin Radoncic <Arminradoncic1@gmail.com>
2023-07-07 00:27:46 -0400

    Refactor close method to properly clean up on exit
    The close method has been refactored to address issues with using
    sys.exit() for code cleanup. Instead, a new is_running flag has been
    introduced in the GameApp class to interrupt the execution of the game
    app when the application is closed prematurely. Additionally, the thread
    responsible for the computer_start() method has been made a data member
    of the class, allowing us to call its join method when the window is
    closed. As a result, the thread no longer needs to be flagged as a daemon.
    
    These changes ensure that the game app closes properly, performing all
    necessary cleanup operations. The code is now more robust and gracefully
    handles early termination of the application.

--- game_app.py
+++ game_app.py
@@ -1,12 +1,14 @@
 from gui import Window
 from maze import Maze
-import players, threading, sys
+import players, threading
 
 class GameApp:
     def __init__(self):
+        self.is_running = True
         self.window = Window(800, 600)
         self.maze = Maze(40, 35, 13, 18, 40, 40, self.window)
         self.player_manager = players.PlayerManager(self.window, self.maze)
+        self.thread = threading.Thread(target=self.player_manager.computer_start)
 
         self.window.start_frame.start_button.config(command=self.wrapper)
         self.window.results_frame.close_button.config(command=self.close)
@@ -17,21 +19,23 @@ class GameApp:
         self.window.wait_for_close()
 
     def close(self):
+        self.is_running = False
         self.player_manager.terminate = True
+        self.window._is_window_running = False
         self.player_manager.human_stop()
         self.window._root.destroy()
-        sys.exit()
+        if self.thread.is_alive():
+            self.thread.join()
 
     def wrapper(self):
         self.window.display_game_frame()
         self.start_game()
 
     def start_game(self):
-        thread = threading.Thread(target=self.player_manager.computer_start, daemon=True)
+        if self.is_running == True:
+            self.thread.start()
+            self.player_manager.human_start()
 
-        thread.start()
-        self.player_manager.human_start()
-
-        thread.join()
-
-        self.window.display_results_frame()
+            if self.is_running == True:
+                self.thread.join()
+                self.window.display_results_frame()
